8d09ecd0c5d20d6acdb2b3c266497097001e2974
Siddharth Priya <s2priya@uwaterloo.ca>
2022-12-29 10:56:42 -0500

    feat: repurpose fatmemmanager

--- lib/seahorn/BvOpSem2.cc
+++ lib/seahorn/BvOpSem2.cc
@@ -2491,6 +2491,8 @@ Bv2OpSemContext::Bv2OpSemContext(Bv2OpSem &sem, SymStore &values,
     } else {
       mem = mkExtraWideMemManager(m_sem, *this, ptrSize, wordSize, UseLambdas);
     }
+  } else if (UseOwnSem) {
+    mem = mkFatMemEWWTManager(sem, *this, ptrSize, wordSize, UseLambdas);
   } else {
     mem = mkRawMemManager(m_sem, *this, ptrSize, wordSize, UseLambdas);
   }

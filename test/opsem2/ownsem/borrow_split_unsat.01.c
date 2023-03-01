a308c30e0ebfda4c74fe3ca98bc6ce4206c91b52
Siddharth Priya <s2priya@uwaterloo.ca>
2023-02-28 21:48:16 -0500

    feat: now new ownsem api so that src/dst can be reused

--- test/opsem2/ownsem/borrow_split_unsat.01.c
+++ test/opsem2/ownsem/borrow_split_unsat.01.c
@@ -14,59 +14,60 @@ typedef struct handle_t {
 
 int main() {
   sea_tracking_on();
-  char *a, *b, *c;
 
-  Handle *h1, *h0, *h00, *h000;
-  Handle *h0000 = (Handle *)malloc(sizeof(Handle));
-  h0000->val = 0;
-  h0000->valid = false;
+  Handle *h0 = (Handle *)malloc(sizeof(Handle));
+  h0->val = 0;
+  h0->valid = false;
 
-  SEA_MKOWN(h000, h0000);
+  SEA_MKOWN(h0);
 
-  SEA_WRITE_CACHE(h00, h000, false);
-  // No need to set fatptr_slot1
-  // A correct non deterministic value is read from slot1
-  // SEA_SET_FATPTR_SLOT1(h0, h00, 0xA);
-  bool *h0b0_valid, *h0b1_valid;
-  SEA_BORROW_OFFSET(h1, h0b0_valid, h00, offsetof(Handle, valid));
+  SEA_WRITE_CACHE(h0, false);
+
+  bool *h0b0_valid;
+  SEA_BORROW_OFFSET(h0b0_valid, h0, offsetof(Handle, valid));
 
   // write to cache and mem
-  SEA_WRITE_CACHE(h0b1_valid, h0b0_valid, true);
-  *h0b1_valid = true;
+  SEA_WRITE_CACHE(h0b0_valid, true);
+  *h0b0_valid = true;
 
-  SEA_DIE(h0b1_valid);
+  SEA_DIE(h0b0_valid);
   bool valToAssert;
-  SEA_READ_CACHE(valToAssert, (char *)h1);
+  char *r = (char *)h0;
+  SEA_READ_CACHE(valToAssert, r);
   sassert(valToAssert == true);
 
-  Handle *h1s, *h1o1, *h1o;
-  Handle *h1b, *h1b1, *h1b2, *h1d;
+  // Handle *h1s, *h1o1, *h1o;
+  // Handle *h1b, *h1b1, *h1b2, *h1d;
 
   bool cacheVal;
-  SEA_READ_CACHE(cacheVal, (char *)h1);
-  h1->valid = cacheVal;
-  SEA_MKSHR(h1s, h1);
-  h1s->val = 1;
-  h1s->valid = false;
+  r = (char *)h0;
+  SEA_READ_CACHE(cacheVal, r);
+  h0->valid = cacheVal;
+  SEA_MKSHR(h0);
+  h0->val = 1;
+  h0->valid = false;
 
-  SEA_MKOWN(h1o, h1s);
-  SEA_WRITE_CACHE(h1o1, h1o, h1o->valid);
-  SEA_READ_CACHE(valToAssert, (char *)h1o1);
+  SEA_MKOWN(h0);
+  SEA_WRITE_CACHE(h0, h0->valid);
+  r = (char *)h0;
+  SEA_READ_CACHE(valToAssert, r);
   sassert(valToAssert == false);
-  SEA_BORROW(h1d, h1b, h1o1);
+  Handle *h1b;
+  SEA_BORROW(h1b, h0);
 
   bool *h1b_valid, *h2b_valid;
-  SEA_BORROW_OFFSET(h1b2, h1b_valid, h1b, offsetof(Handle, valid));
+  SEA_BORROW_OFFSET(h1b_valid, h0, offsetof(Handle, valid));
   // When writing to memory, also write to cache.
-  SEA_WRITE_CACHE(h2b_valid, h1b_valid, false);
-  *h2b_valid = false;
+  SEA_WRITE_CACHE(h1b_valid, false);
+  *h1b_valid = false;
 
-  SEA_DIE(h2b_valid);
+  SEA_DIE(h1b_valid);
 
   // It is valid to read from cache instead of memory
   // since h11u is unique;
   bool v;
-  SEA_READ_CACHE(v, (char *)h1d);
+  r = (char *)h0;
+  SEA_READ_CACHE(v, r);
   sassert(v == false);
   return 0;
 }
--- test/opsem2/ownsem/borrow_split_unsat.02.c
+++ test/opsem2/ownsem/borrow_split_unsat.02.c
@@ -1,4 +1,5 @@
 //; RUN: %sea "%s" --own-sem 2>&1 | OutputCheck %s
+//; RUN: %sea "%s" --own-sem --horn-vcgen-use-ite --horn-vcgen-only-dataflow --horn-bmc-coi --horn-gsa  2>&1 | OutputCheck %s
 // CHECK: ^unsat$
 #include "seahorn/seahorn.h"
 #include <stdbool.h>
@@ -16,62 +17,61 @@ typedef struct handle_t {
 int main() {
   sea_tracking_on();
 
-  Handle *h1, *h00, *h0;
-  Handle *h000 = (Handle *)malloc(sizeof(Handle));
-  h000->val = 0;
-  h000->valid = false;
+  Handle *h0 = (Handle *)malloc(sizeof(Handle));
+  h0->val = 0;
+  h0->valid = false;
 
-  SEA_MKOWN(h00, h000);
+  SEA_MKOWN(h0);
 
-  SEA_WRITE_CACHE(h0, h00, false);
+  SEA_WRITE_CACHE(h0, false);
 
-  // No need to set fatptr_slot1
-  // A correct non deterministic value is read from slot1
-  // SEA_SET_FATPTR_SLOT1(h0, h00, 0xA);
-  bool *h0b0_valid, *h0b1_valid;
-  SEA_BORROW_OFFSET(h1, h0b0_valid, h0, offsetof(Handle, valid));
+  bool *h0b0_valid;
+  SEA_BORROW_OFFSET(h0b0_valid, h0, offsetof(Handle, valid));
 
   // write to cache and mem
-  SEA_WRITE_CACHE(h0b1_valid, h0b0_valid, true);
-  *h0b1_valid = true;
+  SEA_WRITE_CACHE(h0b0_valid, true);
+  *h0b0_valid = true;
 
-  SEA_DIE(h0b1_valid);
+  SEA_DIE(h0b0_valid);
 
   bool valToAssert;
-  SEA_READ_CACHE(valToAssert, (char *)h1);
+  char *r = (char *)h0;
+  SEA_READ_CACHE(valToAssert, r);
 
   sassert(valToAssert == true);
-  Handle *h1s, *h1s1, *h1o, *h1o1;
-  Handle *h1b, *h1b1, *h1b2, *h1d;
+  Handle *h1b;
   if (nd_bool()) {
     // non deterministically choose to have split-borrowed
     // access on h1
     bool cacheVal;
-    SEA_READ_CACHE(cacheVal, (char *)h1);
-    h1->valid = cacheVal; // write cache to mem
-    SEA_MKSHR(h1s1, h1);
-    h1s1->val = 1;
-    h1s1->valid = false;
-    SEA_MKOWN(h1o, h1s1);
-    SEA_WRITE_CACHE(h1o1, h1o, h1o->valid);
-    SEA_READ_CACHE(valToAssert, (char *)h1o1);
+    r = (char *)h0;
+    SEA_READ_CACHE(cacheVal, r);
+    h0->valid = cacheVal; // write cache to mem
+    SEA_MKSHR(h0);
+    h0->val = 1;
+    h0->valid = false;
+    SEA_MKOWN(h0);
+    SEA_WRITE_CACHE(h0, h0->valid);
+    r = (char *)h0;
+    SEA_READ_CACHE(valToAssert, r);
     sassert(valToAssert == false);
-    SEA_BORROW(h1d, h1b, h1o1);
+    SEA_BORROW(h1b, h0);
   } else {
-    SEA_BORROW(h1d, h1b, h1);
+    SEA_BORROW(h1b, h0);
   }
 
-  bool *h1b_valid, *h2b_valid;
-  SEA_BORROW_OFFSET(h1b2, h1b_valid, h1b, offsetof(Handle, valid));
+  bool *h1b_valid;
+  SEA_BORROW_OFFSET(h1b_valid, h1b, offsetof(Handle, valid));
   // When writing to memory, also write to cache.
-  SEA_WRITE_CACHE(h2b_valid, h1b_valid, false);
-  *h2b_valid = false;
-  SEA_DIE(h2b_valid);
+  SEA_WRITE_CACHE(h1b_valid, false);
+  *h1b_valid = false;
+  SEA_DIE(h1b_valid);
 
   // It is valid to read from cache instead of memory
   // since h11u is unique;
   bool v;
-  SEA_READ_CACHE(v, (char *)h1d);
+  r = (char *)h0;
+  SEA_READ_CACHE(v, r);
   sassert(v == false);
 
   return 0;
